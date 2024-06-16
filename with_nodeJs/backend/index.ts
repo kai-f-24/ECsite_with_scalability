68dbf1876cb3dbad1c307659aba43dabdd8788b0
nekokujira24wase <120014921+nekokujira24wase@users.noreply.github.com>
2024-06-16 13:47:41 +0900

    mongoDBを接続

--- with_nodeJs/backend/index.ts
+++ with_nodeJs/backend/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import path from "path";
-import { sampleMerchandiseInfo } from "./db";
+import { MerchandiseBasicInfoList } from "./db";
+import axios from "axios";
+
+const mongoose = require('mongoose');
 
 export const app = express();
 const port  = 3000;
@@ -10,10 +13,11 @@ app.use(express.static(path.join(__dirname, '../frontend')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '..', 'frontend'));
 
-app.get('/', (req, res) => {
-    const sampleData = sampleMerchandiseInfo;
+app.get('/', async(req, res) => {
+    const merchandiseBasicInfoList = await MerchandiseBasicInfoList.find();
+    // console.log(merchandiseBasicInfoList);
     // ここで割引の処理を行う
-    res.render('index', { data: sampleData });
+    res.render('index', { data:merchandiseBasicInfoList });
 });
 
 app.listen(port, () => {
